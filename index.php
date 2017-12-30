d75aa5976104e81fa172d93ab326bb86b5de77a3
Benjamin <Ludwing62@users.noreply.github.com>
2017-12-30 11:23:01 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bienvenido a iNohva Mérida</title>
+        <link rel="stylesheet" href="lib/css/style_menu.css">
+        <?php include 'lib/stylesheet/users_menu.php'?>
+    </head>
+    <body>
+
+    </body>
+</html>
